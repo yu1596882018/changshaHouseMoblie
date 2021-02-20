3bfaba6155645f0f2d513ba3f48ff7a0088dac00
wangxinyu <xinyu.wang@hexasino.com>
2021-02-20 17:51:45 +0800

    feat: 生产环境请求ip地址调整

--- src/config/index.js
+++ src/config/index.js
@@ -1,1 +1,2 @@
-export const requestBaseUrl = 'http://localhost:8899'
+export const requestBaseUrl =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8899' : 'http://39.108.161.237:8899'
