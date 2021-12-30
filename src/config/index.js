61b13c8a8b73d1d0b8d533aa29af7099fb323d4b
wangxinyu <xinyu.wang@hexasino.com>
2021-12-30 15:04:58 +0800

    feat: 请求protocol适应前端页面protocol

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,4 @@
 export const requestBaseUrl =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8899' : 'http://cshouse.api.xinyuexclusive.top'
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8899'
+    : location.protocol + '//cshouse.api.xinyuexclusive.top'
