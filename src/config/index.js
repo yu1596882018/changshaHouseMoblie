cb02cca499f4ea06b12155f7c38b7d0de251507d
xinyu.wang <xinyu.wang@hexasino.com>
2022-05-09 13:18:57 +0800

    feat: 功能优化

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,8 @@
-export const requestBaseUrl =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8899' : location.protocol + '//39.108.161.237:8899'
+let prodHost = 'api.cshouses.top'
+if (location.host === '39.108.161.237:8898') {
+  prodHost = '39.108.161.237:8899'
+}
+
+const prodBaseUrl = location.protocol + '//' + prodHost
+
+export const requestBaseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8899' : prodBaseUrl
