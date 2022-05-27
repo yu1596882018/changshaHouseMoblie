9f9221ac3d6de3275879b917b7b4d0b4818772f9
xinyu.wang <xinyu.wang@hexasino.com>
2022-05-27 10:44:39 +0800

    feat: 备案号展示

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,6 +4,10 @@
       <van-grid-item icon="home-o" text="笔记列表" to="/houseInfoList" />
       <!--<van-grid-item icon="replay" text="笔记数据更新" to="/houseSearch" />-->
     </van-grid>
+
+    <a class="link-icp" href="https://www.beian.gov.cn/portal/registerSystemInfo?recordcode=2022009826">
+      湘ICP备2022009826号-1
+    </a>
   </div>
 </template>
 
@@ -18,4 +22,11 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.link-icp {
+  position: fixed;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
