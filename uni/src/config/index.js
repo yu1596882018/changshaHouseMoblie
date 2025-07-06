0661c432cc9f802c6c492ae898b703e5bd9320e9
王鑫宇 <1596882018@qq.com>
2025-07-06 16:16:46 +0800

    feat: 代码清理

--- uni/src/config/index.js
+++
@@ -1,2 +0,0 @@
-export const requestBaseUrl =
-  process.env.NODE_ENV === 'development' ? 'http://39.108.161.237:8899' : 'http://39.108.161.237:8899'
--- uni/src/lib/index.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import request from './request'
-
-Vue.use(request)
